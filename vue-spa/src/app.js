896bb4fc1908f64f6bc829296e3c2dfbe1fba1cf
Petru Ritivoiu <petru.ritivoiu@cegeka.com>
2019-01-08 20:13:44 +0200

    save point

--- vue-spa/src/app.js
+++ vue-spa/src/app.js
@@ -1,17 +1,11 @@
-import Vue from "vue";
+import Vue from 'vue'
 
-import db from './components/firebaseInit.js';
-let ref = db.ref("Franz Kafka");
-
-ref.on("value", function(snapshot){
-  console.log("Test conexiune baza de date: " + snapshot.val()[0].author);
-});
+import db from './components/firebaseInit.js'
 
 let app = new Vue({
   data: {
-    hello: "Hello World!",
-    numbers: ["one", "two", "three"]
+    hello: 'Hello World!',
   }
-});
+})
 
-export { app };
+export { app }
--- vue-spa/src/components/firebaseInit.js
+++ vue-spa/src/components/firebaseInit.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase' //trebuie instalat separat, nu vine cu webpack
+import firebase from 'firebase'
 import 'firebase/database'
 import firebaseConfig from './firebaseConfig'
 const firebaseApp = firebase.initializeApp(firebaseConfig)
